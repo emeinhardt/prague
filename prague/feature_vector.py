130f82e1c5b5bc468e68469c668473c430087731
Eric Meinhardt <ericmeinhardt@gmail.com>
2020-02-18 14:28:35 -0800

    moved utility functions into feature_vector.py

--- prague/feature_vector.py
+++ prague/feature_vector.py
@@ -7,9 +7,44 @@ import numpy as np
 import scipy.special
 import random
 
-from utility import composable_put, composable_put_along_axis
+# from prague.utility import composable_put, composable_put_along_axis
 
 INT8 = np.int8
+
+###########
+# Utility #
+###########
+
+
+def composable_put(a, ind, v, mode='raise', copy_arg=True):
+    '''A version of np.put that supports functional programming (i.e.
+    compositionality, referential transparency) by returning the array it
+    modifies and (by default) not mutating its primary argument.
+
+    See the documentation for the original function for more details.
+    '''
+    if copy_arg:
+        my_a = a.copy()
+    else:
+        my_a = a
+    np.put(a=my_a, ind=ind, v=v, mode=mode)
+    return my_a
+
+
+def composable_put_along_axis(arr, indices, values, axis=None, copy_arg=True):
+    '''A version of np.put_along_axis that supports functional programming
+    (i.e. compositionality, referential transparency) by returning the array it
+    modifies and (by default) not mutating its primary argument.
+
+    See the documentation for the original function for more details.
+    '''
+    if copy_arg:
+        my_arr = arr.copy()
+    else:
+        my_arr = arr
+    np.put_along_axis(my_arr, indices, values, axis=axis)
+    return my_arr
+
 
 #################
 # Pseudo-typing #
@@ -101,3 +136,4 @@ def load_object_vectors(filepath):
     Loads an ndarray from an .npy file; each row is assumed to be an object.
     '''
     return np.load(filepath)
+
