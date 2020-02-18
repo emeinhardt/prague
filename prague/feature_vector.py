2f600e51b093963574351ca9522c46423ab675ef
Eric Meinhardt <ericmeinhardt@gmail.com>
2020-02-18 14:31:29 -0800

    added code for calculating position in the specification semilattice and constructing elements of the upper closure

--- prague/feature_vector.py
+++ prague/feature_vector.py
@@ -137,3 +137,223 @@ def load_object_vectors(filepath):
     '''
     return np.load(filepath)
 
+
+#############################
+# SPECIFICATION SEMILATTICE #
+#############################
+
+# Slightly redundant with the next function, but also effectively documentation
+# for it, since the next function has a different type signature and is a bit
+# much to grasp all at once.
+def lte_specification(u, v):
+    '''Given two partial feature vectors u, v, this calculates whether
+      u ≤ v
+    in the specification semilattice.
+
+    Intuitively, u ≤ v holds iff v is *less specified* than u. For example,
+        [+nasal]
+    is less specified than [+nasal, +velar], as is
+        [+velar]
+    but [-nasal] is incomparable. (We're not measuring *degree* of
+    specification here.)
+
+    Given two partial feature vectors u, v both with m ternary features, let
+      u[i], v[i]
+    denote the ith feature value for u and v, respectively.
+
+    At the element-level
+        u[i] ≤ v[i]
+    iff
+        (u[i] == v[i]) or (v[i] == 0)
+    i.e.
+      +1 ≤ +1
+      -1 ≤ -1
+       0 ≤  0
+      +1 ≤  0
+      -1 ≤  0
+    in this semilattice ordering, and and this ordering on feature values is
+    extended to vectors in the natural way: this function returns
+        1 iff u[i] ≤ v[i] for all i ∈ [0, m-1]
+    and
+        0 otherwise.
+    '''
+    return ((u == v) | (v == 0)).all()
+
+
+def lte_specification_stack_right(u, M, axis=1):
+    '''Given a partial feature vector u and a matrix (stack) of partial feature
+    vectors M (one vector per row), this efficiently calculates whether
+        u ≤ M[i]
+    for each of the i vectors. In other words, this checks membership of each
+    M[i] in the upper closure of u.
+
+    If
+        u.shape == (m,)
+    and
+        M.shape == (k,m)
+    then
+        lte_specification_stack_right(u,M).shape == (k,)
+    '''
+    return (np.equal(u, M) | np.equal(M, 0)).prod(axis=axis, dtype=INT8)
+
+
+def lte_specification_stack_left(M, u, axis=1):
+    '''Given a partial feature vector u and a matrix (stack) of partial feature
+    vectors M (one vector per row), this efficiently calculates whether
+        M[i] ≤ u
+    for each of the i vectors. In other words, this checks membership of each
+    M[i] in the lower closure of u.
+
+    If
+        u.shape == (m,)
+    and
+        M.shape == (k,m)
+    then
+        lte_specification_stack_left(M,u).shape == (k,)
+    '''
+    return (np.equal(M, u) | np.equal(u, 0)).prod(axis=axis, dtype=INT8)
+
+
+def meet_specification(u=None, v=None, M=None):
+    '''Given two partial feature vectors u,v, returns the unique single partial
+    feature vector that is the greatest lower bound of u and v in the
+    specification semilattice. This will be a vector with every specified value
+    that is specified with exactly the same value in both u and v, and no other
+    specified values.
+
+    Alternately: given a stack of partial feature vectors M (one vector per
+    row), returns the greatest lower bound of the stack in the specification
+    semilattice.
+    '''
+    if u is not None and v is not None:
+        return np.sign(  np.equal(u, v) * (u + v), dtype=INT8)
+    elif M is not None:
+        return np.sign( np.equal.reduce(M, axis=0) * np.sum(M, axis=0), dtype=INT8)
+    else:
+        raise Exception('Provide exactly two vectors u,v or else a stack M.')
+
+
+############################################################
+# GENERATING THE UPPER CLOSURE OF A PARTIAL FEATURE VECTOR #
+############################################################
+
+
+# Useful for testing.
+# Illustrates general idea behind upper_closure.
+def gen_uc(x):
+    '''Generates a random element u of ↑x.
+
+    If k = the number of specified indices in x, then, the generative procedure
+    for creating an element u of  ↑x is as follows:
+      1. A number n≥0 of indices to unspecify is chosen uniformly from [1,k].
+      2. n indices are chosen randomly without replacement from among the
+      specified ones.
+    '''
+    specified_indices = x.nonzero()[0]
+    k = len(specified_indices)
+    num_indices_to_unspecify = random.choice(np.arange(k+1))
+    indices_to_unspecify = np.random.choice(specified_indices,
+                                            size=num_indices_to_unspecify,
+                                            replace=False)
+    u = composable_put(x, indices_to_unspecify, 0)
+    return u
+
+
+# Slightly adapted from https://stackoverflow.com/a/42202157
+def combinations_np(n, k):
+    '''A NumPy-based analogue of itertools.combinations. Assume you have an
+    ndarray x with n elements and want to generate all ways of selecting k
+    indices of x.
+
+    Let r = (n choose k). This function generates an ndarray of shape (r,n)
+    where each column is a set of k indices.
+    '''
+    a = np.ones((k, n-k+1), dtype=int)
+    a[0] = np.arange(n-k+1)
+    for j in range(1, k):
+        reps = (n-k+j) - a[j-1]
+        a = np.repeat(a, reps, axis=1)
+        ind = np.add.accumulate(reps)
+        a[j, ind[:-1]] = 1-reps[1:]
+        a[j, 0] = j
+        a[j] = np.add.accumulate(a[j])
+    return a
+
+
+def n_choose_at_most_k_indices(n, k, asMask=True):
+    '''An extension of combinations_np. Where combinations_np constructs all
+    the ways of choosing a subset of *exactly* k elements from a vector x with
+    n elements, this constructs all ways of choosing ≤ k elements, where each
+    combination is given by a *row* of the output rather than a column.
+
+    If asMask is True:
+    Let r' = (n choose k), and let r = 𝚺_{i=0}^{i=k} (n choose i). This
+    generates an ndarray of shape (r,n) where each row is a binary selection
+    mask indicating the subset of elements of x included in a particular
+    combination of chosen indices.
+
+    If asMask is False:
+    This generates a tuple of k+1 ndarrays, where each ndarray directly
+    indicates selected indices instead of a binary selection mask.
+    '''
+    if not asMask:
+        exact_results_indices = [np.empty((1,0), dtype=np.int64)] + [combinations_np(n,i).T
+                                                                     for i in np.arange(1, k+1)]
+        return tuple(exact_results_indices)
+    mask = np.concatenate([np.zeros((1,n), dtype=INT8)] +
+                          [composable_put_along_axis(np.zeros((int(scipy.special.binom(n,i)), n),
+                                                              dtype=INT8),
+                                                     combinations_np(n,i).T,
+                                                     1,
+                                                     axis=1,
+                                                     copy_arg=False)
+                           for i in np.arange(1, k+1)]).astype(INT8)
+    return mask
+
+
+def upper_closure(x, strict=False):
+    '''The upper closure ↑x of a pfv x is the set of (optionally strictly) less
+    specified vectors. If X is the set of all partially specified feature
+    vectors, then
+        ↑x = {y ∈ X | x ≤ y}
+
+    These are returned as a stack of partial feature vectors (one vector per
+    row), with no guarantees about the order of such vectors.
+
+
+    If m is the total number of features that could be specified, then there
+    are O(𝚺_i=1^i=m m choose i) elements in this set.
+
+    If k ≤ m is the exact number of features that are specified in pfv x, then
+    there are exactly (𝚺_i=1^i=k k choose i) = 2^k elements in this set.
+    '''
+    specified_indices = x.nonzero()[0]
+    k = len(specified_indices)
+
+    unspecified_indices = (x == 0).nonzero()[0]
+
+    #There is one element in ↑x for each possible combination of specified
+    # indices = a combination of indices of x that can be *un*specified.
+    combinations_of_indices_to_unspecify = n_choose_at_most_k_indices(k, k,
+                                                                      asMask=True)
+
+    #The goal is to efficiently generate ↑x via Hadamard product of x with a
+    # stack of vectors representing 'masks' that each cause a different kind
+    # of unspecification.
+
+    #Create a mask with the same shape as x and selection indices in the
+    # right place.
+    offsets = np.arange(len(unspecified_indices))
+    selection_mask = np.insert(combinations_of_indices_to_unspecify,
+                               obj = unspecified_indices - offsets,
+                               values = 0, #0s go in the indices whose specification won't be changed
+                               axis = 1)   #masks are stacked vertically
+
+    #By negating the selection mask, we get a stack of vectors that each have
+    # 0s where we want to erase (unspecify) a value.
+    eraser_mask = np.logical_not(selection_mask).astype(INT8)
+
+    my_upper_closure = (x * eraser_mask).astype(INT8)
+    if strict:
+        my_upper_closure = my_upper_closure[1:] #pop first element (==x)
+    return my_upper_closure
