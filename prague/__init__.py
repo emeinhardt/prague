f13f2838d4b5b28310cd6559ca958a58985d1705
Eric Meinhardt <ericmeinhardt@gmail.com>
2020-02-18 20:03:55 -0800

    added hashable array export

--- prague/__init__.py
+++ prague/__init__.py
@@ -1,10 +1,14 @@
+from prague.convert import load_objects
+import prague.convert
 from prague.feature_vector import make_random_objects, load_object_vectors
+
+from prague.feature_vector import HashableArray
+
+from prague.feature_vector import lte_specification as lte
+from prague.feature_vector import meet_specification as meet
 from prague.feature_vector import upper_closure, lower_closure
+
 from prague.feature_vector import objects_to_extension_vector, extension_vector_to_objects
 from prague.feature_vector import extension, extensions
 from prague.feature_vector import get_pfvs_whose_extension_contains
 from prague.feature_vector import get_pfvs_whose_extension_is_exactly
-from prague.feature_vector import meet_specification as meet
-from prague.feature_vector import lte_specification as lte
-from prague.convert import load_objects
-import prague.convert
