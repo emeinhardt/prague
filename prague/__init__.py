32753d9e0db942b0bb629eabadda57b91114b307
Eric Meinhardt <ericmeinhardt@gmail.com>
2020-02-27 10:13:13 -0800

    updated prague's init file to export distance, transformation, and to_feature_dict functions'

--- prague/__init__.py
+++ prague/__init__.py
@@ -4,11 +4,18 @@ from prague.feature_vector import make_random_objects, load_object_vectors
 
 from prague.feature_vector import HashableArray
 
+from prague.feature_vector import to_feature_dict
+
 from prague.feature_vector import lte_specification as lte
 from prague.feature_vector import meet_specification as meet
 from prague.feature_vector import upper_closure, lower_closure
+from prague.feature_vector import get_children, get_parents
 
 from prague.feature_vector import objects_to_extension_vector, extension_vector_to_objects
 from prague.feature_vector import extension, extensions
 from prague.feature_vector import get_pfvs_whose_extension_contains
 from prague.feature_vector import get_pfvs_whose_extension_is_exactly
+
+
+from prague.feature_vector import hamming, delta_right, delta_down
+from prague.feature_vector import linear_transform, despec
